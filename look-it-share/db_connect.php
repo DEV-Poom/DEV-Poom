3a16751caa6a9471e727ce2f69348c24e7fffd89
do not tell <95093097+DEV-Poom@users.noreply.github.com>
2022-01-07 19:16:41 +0700

    Delete look-it-share directory

--- look-it-share/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $mysqli = new mysqli("localhost", "root", "") or die("cannot comppect database. ");
-
-    $mysqli->select_db("look_it_share")or die("cannot select database. ");
-
-    if(!$mysqli) echo "Cannot Connect Database!!<br>"; 
-?>
